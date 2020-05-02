91dcad3b740daa3f3ff56c4ffc2872ecb04b0757
Igor Kalevatykh <kalevatykh@gmail.com>
2020-05-02 19:29:14 +0200

    Bump version: 0.2.0-dev → 0.2.0

--- panda3d_viewer/__init__.py
+++ panda3d_viewer/__init__.py
@@ -3,7 +3,7 @@ This package contains Viewer, a simpe and efficient
 cross-platform 3D viewer based on Panda3D.
 """
 
-__version__ = '0.2.0-dev'
+__version__ = '0.2.0'
 
 from .viewer import Viewer
 from .viewer_config import ViewerConfig
