9931c1ced800a120ea3db295d84dcd831488cf8b
Igor Kalevatykh <kalevatykh@gmail.com>
2020-11-06 20:14:02 +0100

    Bump version: 0.3.0 → 0.3.1-dev

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='panda3d_viewer',
-    version='0.3.0',
+    version='0.3.1-dev',
     description='Easy-to-use python 3D graphics viewer',
     long_description=long_description,
     long_description_content_type='text/markdown',
